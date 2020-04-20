f2bf41484cb99b8a09884f31639a11013bd737be
Hayoun Lee <sidhlee@gmail.com>
2020-04-20 20:43:29 +0900

    Apply React.memo to Modal

--- src/common/ui/Modal.js
+++ src/common/ui/Modal.js
@@ -11,6 +11,7 @@ Modal.propTypes = {
 };
 
 function Modal({ className, show, closeModal, children }) {
+  console.log('Modal rendered!');
   return (
     <div className={className}>
       <Backdrop show={show} clicked={closeModal} />
@@ -22,26 +23,37 @@ function Modal({ className, show, closeModal, children }) {
 const slideInOut = (props) =>
   props.show ? 'translateY(0)' : 'translateY(-100vh)';
 
-export default styled(Modal)`
-  ${(props) => css`
-    section {
-      position: fixed;
-      width: 80%;
-      max-width: 500px;
-      min-height: 10em;
-      border-radius: 0.5em;
-      top: 30%;
-      left: 50%;
-      transform: translateX(-50%) ${slideInOut};
-      transition: transform 0.3s ease-in-out;
-      background: var(--cl-light);
-      z-index: var(--z-modal);
-      padding: 3em 1em 2em;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      ${shadow}
-    }
-  `}
-`;
+// if show props is the same, use memoized version
+function areEqual(prevProps, nextProps) {
+  return nextProps.show === prevProps.show;
+}
+
+// You need 2nd argument comparison function to React.memo here
+// because the parent component: BurgerBuilder passes handler function
+// which is re-created every time the component renders
+export default React.memo(
+  styled(Modal)`
+    ${(props) => css`
+      section {
+        position: fixed;
+        width: 80%;
+        max-width: 500px;
+        min-height: 10em;
+        border-radius: 0.5em;
+        top: 30%;
+        left: 50%;
+        transform: translateX(-50%) ${slideInOut};
+        transition: transform 0.3s ease-in-out;
+        background: var(--cl-light);
+        z-index: var(--z-modal);
+        padding: 3em 1em 2em;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        ${shadow}
+      }
+    `}
+  `,
+  areEqual
+);
